937b1852551e1c02f2b645d517af7afab4c73a15
0427ryan <70810066+0427ryan@users.noreply.github.com>
2020-10-19 10:23:37 +0800

    change card play

---
+++ java/Cardplay/src/Player.java
@@ -0,0 +1,12 @@
+
+public class Player {
+    String name;
+
+    public Player(){
+        this.name = "unnamed player";
+    }
+    public Player(String name){
+        this.name = name;
+    }
+    
+}
