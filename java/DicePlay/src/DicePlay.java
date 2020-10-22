0615a1c9eef57ea9b2ae3eb2c1a66dc0786fefb2
0427ryan <70810066+0427ryan@users.noreply.github.com>
2020-10-22 15:10:41 +0800

    Update DicePlay.java

--- java/DicePlay/src/DicePlay.java
+++ java/DicePlay/src/DicePlay.java
@@ -5,11 +5,14 @@ public class DicePlay {
     }
     
     public void play(){
+
         Dice d1 = new Dice();
         Dice d2 = new Dice();
         int sum = d1.getNextNumber() + d2.getNextNumber();
+
         System.out.println("d1:" + d1.getState());
         System.out.println("d2:" + d2.getState());
+        
         if(sum == 2 || sum == 3 || sum == 12){
             System.out.println("lose");
             return;
