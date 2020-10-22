ac4d041166aeb9f3f285ce8b01d4c34a9f55282d
0427ryan <70810066+0427ryan@users.noreply.github.com>
2020-10-22 15:10:16 +0800

    Create DicePlay.java

---
+++ java/DicePlay/src/DicePlay.java
@@ -0,0 +1,33 @@
+
+public class DicePlay {
+
+    public DicePlay(){
+    }
+    
+    public void play(){
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        int sum = d1.getNextNumber() + d2.getNextNumber();
+        System.out.println("d1:" + d1.getState());
+        System.out.println("d2:" + d2.getState());
+        if(sum == 2 || sum == 3 || sum == 12){
+            System.out.println("lose");
+            return;
+        }
+        if(sum == 7 || sum == 11){
+            System.out.println("win");
+            return;
+        }
+        while(sum != 7){
+            sum = d1.getNextNumber() + d2.getNextNumber();
+            System.out.println("d1:" + d1.getState());
+            System.out.println("d2:" + d2.getState());
+            if(d1.getState() == d2.getState()){
+                System.out.println("win");
+                return;
+            }
+        }
+        System.out.println("lose");
+    }
+
+}
